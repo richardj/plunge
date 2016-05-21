a07e0989876085f0eb64901219de42bf87ff3024
Richard Japenga <treurmars@gmail.com>
2016-05-21 19:27:06 +0200

    works now with data attributes and looks at the nextElementSibling for now

--- swoop.js
+++ swoop.js
@@ -1,6 +1,6 @@
 (function() {
   var assignHandlers = function() {
-    document.querySelector('body').addEventListener('click', modal.open, false);
+    document.querySelector('body').addEventListener('click', dropdown.open, false);
   };
 
   var findAncestor = function(el, cls) {
@@ -11,44 +11,64 @@
     return el;
   };
   
-  var modal = {
+  var dropdown = {
     open: function(event) {
-      var clicked = findAncestor(event.target, 'modal');
+      var root = document.documentElement;
+      var clicked = findAncestor(event.target, 'dropdown');
+      var dropElement = event.target.nextElementSibling;
 
-      if (event.target.classList.contains('button')) {
-        modal.closeAll();
-        document.body.classList.add('modal-active');
-        modal.content(event);
+      if (event.target.dataset.swTrigger && !event.target.classList.contains('sw-trigger-active')) {
+        dropdown.closeAll();
+
+        root.classList.add('sw-active');
+        event.target.classList.add('sw-trigger-active');
+        dropElement.classList.add('sw-active');
+        dropdown.position(dropElement);
       }
-      else if (document.body.classList.contains('modal-active') && clicked === null) {
-        modal.closeAll();
+      else if (root.classList.contains('sw-active') && clicked === null) {
+        dropdown.closeAll();
       }
       else {
         return;
       }
+    },
+    position: function(el) {
+      var posLeft = event.target.offsetLeft;
+      var posTop = event.target.offsetTop + event.target.clientHeight + 10;
+
+      el.style.top = posTop + "px";
+      el.style.left = posLeft + "px";
     },
     content: function(event) {
       var posLeft = event.target.offsetLeft;
       var posTop = event.target.offsetTop + event.target.clientHeight + 10;
 
       var el = document.createElement('div');
-      el.classList.add('modal');
+      el.classList.add('dropdown');
       el.innerHTML = '<h3>lorem ipsum</h3>';
       el.style.top = posTop + "px";
       el.style.left = posLeft + "px";
     
       document.body.insertBefore(el, document.body.childNodes[0]); 
     },
+
     close: function(element) {
       element.parentNode.removeChild(element);  
     },
+
+    hide: function(element) {
+      element.classList.remove('sw-active');
+      element.classList.remove('sw-trigger-active');
+    },
+
     closeAll: function() {
-      var elements = document.getElementsByClassName('modal');
+      var elements = document.querySelectorAll('.dropdown, .sw-trigger-active');
+      console.log(elements);
     
       for (var i = 0; i < elements.length; i++) {
-        modal.close(elements[i]);
+        dropdown.hide(elements[i]);
       }
-      document.body.classList.remove('modal-active');
+      document.documentElement.classList.remove('sw-active');
     }
   };
   
